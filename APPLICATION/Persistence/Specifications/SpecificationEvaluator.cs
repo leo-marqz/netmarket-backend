4947376f8efe4a62701f5d93514d6620c9bd4387
Elmer Marquez <leomarqz2020@gmail.com>
2024-08-05 20:10:17 -0600

    se configuraron los metodos de ordenamiento en SpecificationEvaluator

--- APPLICATION/Persistence/Specifications/SpecificationEvaluator.cs
+++ APPLICATION/Persistence/Specifications/SpecificationEvaluator.cs
@@ -13,6 +13,16 @@ namespace APPLICATION.Persistence.Specifications
                 query = query.Where(specification.Criteria);
             }
 
+            if(specification.OrderBy != null)
+            {
+                query = query.OrderBy(specification.OrderBy);
+            }
+
+            if(specification.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(specification.OrderByDescending);
+            }
+
             query = specification.Includes.Aggregate(query, (current, include) => current.Include(include));
 
             return query; 
