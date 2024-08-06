9a2c727d2fe2ddeb050bfedc43d7eadf5ff74520
Elmer Marquez <leomarqz2020@gmail.com>
2024-08-05 20:06:12 -0600

    agregando metodos de ordenamiento en las consultas

--- APPLICATION/Persistence/Specifications/BaseSpecification.cs
+++ APPLICATION/Persistence/Specifications/BaseSpecification.cs
@@ -8,10 +8,7 @@ namespace APPLICATION.Persistence.Specifications
 {
     public class BaseSpecification<T> : ISpecification<T>
     {
-        public BaseSpecification()
-        {
-            
-        }
+        public BaseSpecification() { }
 
         public BaseSpecification(Expression<Func<T, bool>> criteria)
         {
@@ -21,12 +18,30 @@ namespace APPLICATION.Persistence.Specifications
         public Expression<Func<T, bool>> Criteria { get; }
 
         public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
-
+        
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
             Includes.Add(includeExpression);
         }
 
 
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
+        }
+
+
+        public int Take { get; set; }
+
+        public int Skip { get; set; }
     }
 }
--- APPLICATION/Persistence/Specifications/ISpecification.cs
+++ APPLICATION/Persistence/Specifications/ISpecification.cs
@@ -8,5 +8,9 @@ namespace APPLICATION.Persistence.Specifications
     {
         Expression<Func<T, bool>> Criteria { get; }
         List<Expression<Func<T, object>>> Includes { get; }
+        Expression<Func<T, object>> OrderBy { get; }
+        Expression<Func<T, object>> OrderByDescending { get; }
+        int Take { get; }
+        int Skip { get; }
     }
 }
