ab01b80182e62138cffc26dd5074ebf977edac53
Elmer Marquez <leomarqz2020@gmail.com>
2024-08-05 19:56:47 -0600

    se agrego middleware para controlar errores en transacciones

---
+++ API/Handlers/CodeErrorException.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Handlers
+{
+    public class CodeErrorException : CodeErrorResponse
+    {
+        //for middleware
+        public CodeErrorException(int statusCode, string message = default, string details = default) 
+        : base(statusCode, message)
+        {
+            Details = details;
+        }
+
+        public string Details { get; set; }
+    }
+}
---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using API.Handlers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionMiddleware> logger;
+        private readonly IHostEnvironment env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            this.next = next;
+            this.logger = logger;
+            this.env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await this.next(context);
+            }
+            catch (System.Exception ex)
+            {
+                this.logger.LogError(ex, ex.Message);
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                var response = this.env.IsDevelopment()
+                    ? new CodeErrorException(StatusCodes.Status500InternalServerError, ex.Message, ex.StackTrace.ToString())
+                    : new CodeErrorException(StatusCodes.Status500InternalServerError);
+
+                var result = System.Text.Json.JsonSerializer.Serialize(response);
+
+                await context.Response.WriteAsync(result);
+            }
+        }
+    }
+}
